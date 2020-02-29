177013af747043c8b4f27a2dc0370edb094fe5a0
Eduardo Lacerda <lacerda.eduardo92@gmail.com>
2020-02-29 14:27:56 -0300

    Project structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SwStarship
+# Sw Starships Resupply Calculator 
+
+### How is this project organized
+
+### How to run this project
+
+### How to execut unit tests
+`dotnet test`
---
+++ test/SwStarship.Core.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using FluentAssertions;
+
+namespace SwStarship.Core.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            "".Should().Be(string.Empty);
+        }
+    }
+}
